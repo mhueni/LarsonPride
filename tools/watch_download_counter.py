0ed56fcfff006748184251297841c0a35c0ce3a1
b--ger <rgaensli@gmail.com>
2017-08-07 14:07:19 +0200

    Added toools for hatchery monitoring

---
+++ tools/watch_download_counter.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+
+
+def main():
+    response = requests.request('GET', 'https://badge.sha2017.org/eggs/list/json')
+
+    # print(response)
+    data = response.json()
+    # print(data)
+    hitlist = sorted(data, key=lambda x: x['download_counter'], reverse=True)
+    
+    for h in hitlist:
+        print(h['download_counter'], h['name'])
+    
+    
+
+if __name__ == '__main__':
+    main()
